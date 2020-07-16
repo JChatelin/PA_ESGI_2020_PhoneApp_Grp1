f7147ee4c9efbdf880e766b507e5584c63b83766
ThomFDV <thomas_ferreira_93330@hotmail.fr>
2020-07-16 09:00:27 +0200

    Update playlist

--- app/src/main/java/com/esgipa/smartplayer/ui/viewmodel/PlaylistSharedViewModel.java
+++ app/src/main/java/com/esgipa/smartplayer/ui/viewmodel/PlaylistSharedViewModel.java
@@ -46,11 +46,10 @@ public class PlaylistSharedViewModel extends ViewModel {
     }
 
     private void loadPlaylists() {
-        /*for (Playlist playlist: loadedPlaylist) {
-            if(!list.contains(playlist)) {
-                list.add(playlist);
-            }
-        }*/
-        playlistList.postValue(loadedPlaylist);
+        list.clear();
+        for (Playlist playlist: loadedPlaylist) {
+            list.add(playlist);
+        }
+//        playlistList.postValue(loadedPlaylist);
     }
 }
